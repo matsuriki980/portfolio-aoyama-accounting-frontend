dff859d1206ea2c5f8c2688906d0512ce733328d
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-05-16 10:10:39 +0900

    add: drawer画像hover切り替え処理を追加

--- js/modules/header/render.js
+++ js/modules/header/render.js
@@ -7,6 +7,7 @@ import { headerState } from "./state.js"; // header background・drawer状態を
 const header = document.querySelector(".js-header");
 const isSubPage = header.classList.contains("l-header--sub-page"); // 下層ページならtrueを返す
 const drawer = document.querySelector(".js-drawer");
+const drawerImgs = document.querySelectorAll(".js-drawer__img");
 
 export const headerRender = () => {
   // 要素が存在しなければ処理停止
@@ -38,4 +39,21 @@ export const headerRender = () => {
     "l-header--sub-page",
     isSubPage && !headerState.isDrawerOpen,
   );
+
+  // ==========================================================
+  // drawer hover img
+  // ==========================================================
+
+  // 一旦active削除
+  drawerImgs.forEach((img) => {
+    img.classList.remove("is-active");
+  });
+
+  // stateと一致する画像を取得
+  const activeImg = document.querySelector(
+    `.js-drawer__img[data-img="${headerState.activeDrawerImg}"]`,
+  );
+
+  // active付与
+  activeImg.classList.add("is-active");
 };
